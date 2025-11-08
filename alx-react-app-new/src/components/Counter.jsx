1217a49e98cc622ff9819564f87089d55819b0a0
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-08 18:42:08 +0100

    Fix typo

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -5,14 +5,14 @@ import { useState } from "react";
 
 function Counter() {
     
-const [count, setcount] = useState(0)
+const [Count, setCount] = useState(0)
 
     return (
         <div>
-            <p>Current count :{count}</p>
-            <button onClick={() => {setcount(count+1)}}>Incrementing</button>
-            <button onClick={() => {setcount(count-1)}}>Decrementing</button>
-            <button onClick={() =>{setcount(0)}}>Reset</button>
+            <p>Current count :{Count}</p>
+            <button onClick={() => {setCount(Count+1)}}>Incrementing</button>
+            <button onClick={() => {setCount(Count-1)}}>Decrementing</button>
+            <button onClick={() =>{setCount(0)}}>Reset</button>
         </div>
     );
 };
