bdc10c7009639b425973fcff32a2bca7d53a41c2
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-11-30 18:50:58 +0100

    rendering Userprofile from App.jsx and remove it from Main.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <UserProfile />
+      </div>
       <div className='flex justify-center items-center gap-8'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
