484a085e3cecc212e1dfd71963a09a29d15194fa
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-12 15:47:30 +0100

    Set up the project and initialize the RegistrationForm

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,32 +1,55 @@
-import React from "react";
+import { useState } from "react"
+import { resumeAndPrerender } from "react-dom/static";
 
 const RegistrationForm = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
 
-    const HandleSubmit(e) {
-        e.prevent.default;
 
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Username: ${username}\nEmail: ${email}\nPassword: ${password}`);
     }
 
     return (
-        <form onSubmit={}>
-            <input 
-            type="text"
-            placeholder="Enter a name"
-            />
-            <input 
-            type="email" 
-            placeholder="Enter an email"
-            />
-            <input 
-            type="password"
-            placeholder="Create a password"
-            />
-            <input 
-            type="password"
-            placeholder="Confirm the password"
-            />
-            <button onClick={HandleSubmit}>Submit</button>
-        </form>
+        <div>
+            <h2>Register</h2>
+            <form onSubmit={HandleSubmit}>
+                <div>
+                    <h3>Username : </h3>
+                    <input 
+                    type="text"
+                    name="username"
+                    placeholder="Enter a name"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <h3>Email : </h3>
+                    <input 
+                    type="email" 
+                    placeholder="Enter an email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <h3>Password : </h3>
+                    <input 
+                    type="password"
+                    placeholder="Create a password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    />                    
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     );
 
 }
