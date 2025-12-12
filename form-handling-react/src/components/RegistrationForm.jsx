59285414c462c1db670a2a41e6c232d620f24ed0
EL FALEH JALAL <elfalehjalal.01@gmail.com>
2025-12-12 16:08:46 +0100

    Set up the project and initialize the RegistrationForm

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,17 +1,34 @@
 import { useState } from "react"
-import { resumeAndPrerender } from "react-dom/static";
 
 const RegistrationForm = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState({});
-    const [isSubmitted, setIsSubmitted] = useState(false);
-
+    const [errors, setErrors] = useState({});
 
     const HandleSubmit = (e) => {
         e.preventDefault();
-        alert(`Username: ${username}\nEmail: ${email}\nPassword: ${password}`);
+        const newErrors = {};
+        if(!username) {
+            newErrors.username = 'Enter a Username' ;            
+        };
+        if(!email) {
+            newErrors.email = 'Enter a valid email';
+        };
+        if(!password) {
+            newErrors.password = 'enter a password';
+        };
+
+        if (Object.keys(newErrors).length > 0 ) {
+            setErrors(newErrors);
+            return;
+        }
+
+        alert('Submitted!');
+        setUsername('');
+        setEmail('');
+        setPassword('');
+        setErrors({});
     }
 
     return (
@@ -19,7 +36,7 @@ const RegistrationForm = () => {
             <h2>Register</h2>
             <form onSubmit={HandleSubmit}>
                 <div>
-                    <h3>Username : </h3>
+                    <label>Username : </label>
                     <input 
                     type="text"
                     name="username"
@@ -27,24 +44,27 @@ const RegistrationForm = () => {
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     />
+                    {errors.username && <p style={{color: 'red'}}>{errors.username}</p>}
                 </div>
                 <div>
-                    <h3>Email : </h3>
+                    <label>Email : </label>
                     <input 
                     type="email" 
                     placeholder="Enter an email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     />
+                    {errors.email && <p style={{color: 'red'}}>{errors.email}</p>}
                 </div>
                 <div>
-                    <h3>Password : </h3>
+                    <label>Password : </label>
                     <input 
                     type="password"
                     placeholder="Create a password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    />                    
+                    />   
+                    {errors.password && <p style={{color: 'red'}}>{errors.password}</p>}                 
                 </div>
 
                 <button type="submit">Submit</button>
